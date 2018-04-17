71b4563762a4e9ea8f64966d675cec538eca3f39
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-04-17 13:35:14 -0400

    Added code for group Pokedex project

---
+++ projects/03-group-pokedex/listeners.js
@@ -0,0 +1,29 @@
+console.log("listeners connected")
+
+$(".pokemon-link").on("click", function(e){
+    let parentClass = $(this).parent().parent().attr('class')
+    let thisPokemon = $(this).attr('id')
+    let area
+    let trainer
+    let animationDirectionClass
+
+    console.log(thisPokemon)
+    
+
+    if(/olga/.test(parentClass)){
+        area = "1"
+        trainer = olga
+        animationDirectionClass = 'moveRight'
+    } else{
+        area = "2"
+        trainer = david
+        animationDirectionClass = 'moveLeft'
+    }
+
+    $(`#pokemon${area}`).html(`<div class=${animationDirectionClass}><div class="yAxis"><img id="pokemonImg${area}" src="${trainer.myPokemon[thisPokemon].frontImg}" alt="pokemon${area}"></div></div>`)
+
+    $(`#pokemon-info-${area}`).html(`<div id="info"><div><span class="statHeading">hp</span>: ${trainer.myPokemon[thisPokemon].hp}</div><div><span class="statHeading">attack</span>: ${trainer.myPokemon[thisPokemon].attack}</div><div><span class="statHeading">defense</span>: ${trainer.myPokemon[thisPokemon].defense}</div><div><span class="statHeading">abilities</span>: <span id="abilities-${area}"></span></div></div>`)
+
+    trainer.myPokemon[thisPokemon].abilities.forEach(function(ability) {$(`#abilities-${area}`).append(ability + "    &nbsp;&nbsp;&nbsp;&nbsp;")})
+
+})
